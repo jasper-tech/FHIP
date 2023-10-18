eef60ea99f5cd66712811d2a6d6ddbf645f7e85c
jasper tech <curdyj78@gmail.com>
2023-10-17 12:16:19 -1200

    Today's commit

--- start.py
+++ start.py
@@ -1,17 +1,22 @@
 import os
 
+#Read contents from a file
 file = open("C:/Users/Administrator/OneDrive/Pictures/Documents/Custom Project Designs/FHIP/First.txt","r")
 print(file.read())
 file.close()
 
+#Read contents up to index 7 from file
 file = open("C:/Users/Administrator/OneDrive/Pictures/Documents/Custom Project Designs/FHIP/First.txt","r")
 print(file.read(7))
 file.close()
 
+#Read contents line by line
 file = open("C:/Users/Administrator/OneDrive/Pictures/Documents/Custom Project Designs/FHIP/First.txt","r")
 print(file.readline())
 file.close()
 
+
+#Read contents line by line in an array view
 file = open("C:/Users/Administrator/OneDrive/Pictures/Documents/Custom Project Designs/FHIP/First.txt","r")
 print(file.readlines())
 file.close()
