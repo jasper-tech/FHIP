03a796df94dc09da9258df73f3edcd2e88900fbb
jasper tech <curdyj78@gmail.com>
2023-10-17 12:46:47 -1200

    Another way to check if contents exist

---
+++ line.py
@@ -0,0 +1,6 @@
+import os
+
+file = open("C:/Users/Administrator/OneDrive/Pictures/Documents/Custom Project Designs/FHIP/First.txt","r")
+for line in file:
+    print(file.readlines())
+file.close()
