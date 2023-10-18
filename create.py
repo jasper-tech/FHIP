f2c1c49faac30e518073b94c99a22f44e6358216
jasper tech <curdyj78@gmail.com>
2023-10-17 12:32:41 -1200

    a new file has been created using the "x" syntax

---
+++ create.py
@@ -0,0 +1,7 @@
+import os
+
+#Creating a new file with python
+file = open("C:/Users/Administrator/OneDrive/Pictures/Documents/Custom Project Designs/FHIP/newfile.txt","x")
+#Write into the file
+file.write("Sunshine")
+file.close()
